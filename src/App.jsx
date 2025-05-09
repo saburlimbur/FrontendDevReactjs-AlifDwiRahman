1d0dd30c996b4fb71b2b44f1a654b83c79989377
Alif Dwi Rahman <alifdwirahman54@gmail.com>
2025-05-09 11:42:51 +0700

    feat: improve responsive layout in all device

--- src/App.jsx
+++ src/App.jsx
@@ -24,9 +24,13 @@ function App() {
     return <ErrorElement />;
   }
 
-  const filteredByCategory = category ? restaurantList.restaurants.filter((restaurant) => restaurant.city === category) : restaurantList.restaurants;
+  const filteredByCategory = category
+    ? restaurantList.restaurants.filter((restaurant) => restaurant.city === category)
+    : restaurantList.restaurants;
 
-  const filteredByRating = ratingFilter ? filteredByCategory.filter((restaurant) => restaurant.rating >= parseFloat(ratingFilter)) : filteredByCategory;
+  const filteredByRating = ratingFilter
+    ? filteredByCategory.filter((restaurant) => restaurant.rating >= parseFloat(ratingFilter))
+    : filteredByCategory;
 
   const handleCategoryChange = (e) => {
     setCategory(e.target.value);
@@ -47,24 +51,30 @@ function App() {
   };
 
   return (
-    <main className="min-h-screen flex flex-col gap-10 container mx-auto py-10">
-      <div className="flex flex-col gap-5 px-10">
+    <main className="min-h-screen flex flex-col gap-10 px-4 sm:px-6 md:px-8 lg:px-10 xl:px-12 py-10 max-w-screen-xl mx-auto">
+      <div className="flex flex-col gap-5">
         <h1 className="text-4xl font-medium">Restaurants</h1>
-        <p className="text-gray-400 font-medium text-base max-w-[600px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <p className="text-gray-400 font-medium text-base max-w-[600px]">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
       </div>
 
       <header className="py-6 border-y border-y-gray-200">
-        <nav className="flex items-center justify-between">
-          <div className="px-10 flex gap-8 items-center">
+        <nav className="flex flex-col lg:flex-row gap-4 lg:items-center lg:justify-between">
+          <div className="flex flex-col sm:flex-row flex-wrap gap-4 items-start sm:items-center">
             <p className="text-gray-600 font-normal text-base">Filter By:</p>
 
-            <div className="flex items-center gap-1.5 border-b border-b-gray-200 w-[120px] py-1.5">
+            <div className="flex items-center gap-1.5 border-b border-b-gray-200 w-full sm:w-auto py-1.5">
               <input type="checkbox" />
               <span className="text-gray-600 font-normal text-base">Open Now</span>
             </div>
 
-            <div className="flex items-center gap-1 border-b border-b-gray-200 py-1.5">
-              <select className="rounded-md text-gray-600 font-normal text-base w-[150px] cursor-pointer" value={ratingFilter} onChange={handleRatingFilterChange}>
+            <div className="flex items-center gap-1 border-b border-b-gray-200 py-1.5 w-full sm:w-auto">
+              <select
+                className="rounded-md text-gray-600 font-normal text-base w-full sm:w-[150px] cursor-pointer"
+                value={ratingFilter}
+                onChange={handleRatingFilterChange}
+              >
                 <option value="">Rating</option>
                 <option value="4">$$$$</option>
                 <option value="3">$$$</option>
@@ -73,29 +83,36 @@ function App() {
               </select>
             </div>
 
-            <div className="flex items-center gap-1 border-b border-b-gray-200 py-1.5">
-              <select className="rounded-md text-gray-600 font-normal text-base w-[250px] cursor-pointer" value={category} onChange={handleCategoryChange}>
+            <div className="flex items-center gap-1 border-b border-b-gray-200 py-1.5 w-full sm:w-auto">
+              <select
+                className="rounded-md text-gray-600 font-normal text-base w-full sm:w-[250px] cursor-pointer"
+                value={category}
+                onChange={handleCategoryChange}
+              >
                 <option value="" disabled>
                   Categories
                 </option>
-                {restaurantList?.restaurants.map((restaurant, index) => (
-                  <option key={index} value={restaurant.city} className="cursor-pointer">
-                    {restaurant.city}
+                {[...new Set(restaurantList?.restaurants.map((r) => r.city))].map((city, index) => (
+                  <option key={index} value={city}>
+                    {city}
                   </option>
                 ))}
               </select>
             </div>
           </div>
 
-          <div className="max-w-md">
-            <Button className="py-2 px-8 text-base font-medium border border-gray-200 rounded-md uppercase cursor-pointer hover:bg-gray-50 text-gray-400" onClick={handleClearAll}>
+          <div className="w-full sm:w-auto">
+            <Button
+              className="w-full sm:w-auto py-2 px-6 text-base font-medium border border-gray-200 rounded-md uppercase cursor-pointer hover:bg-gray-50 text-gray-400"
+              onClick={handleClearAll}
+            >
               Clear All
             </Button>
           </div>
         </nav>
       </header>
 
-      <section className="flex flex-col gap-8 px-10">
+      <section className="flex flex-col gap-8">
         <h2 className="text-3xl font-medium">All Restaurants</h2>
 
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 w-full">
@@ -104,7 +121,11 @@ function App() {
 
             return (
               <Card key={id} className="overflow-hidden w-full h-auto flex flex-col justify-between">
-                <Card.Image className="h-48 w-full object-cover hover:scale-105 transition-transform duration-200" src={RestaurantService.getImageUrl(pictureId, 'medium')} alt={name} />
+                <Card.Image
+                  className="h-48 w-full object-cover hover:scale-105 transition-transform duration-200"
+                  src={RestaurantService.getImageUrl(pictureId, 'medium')}
+                  alt={name}
+                />
                 <div className="py-4 flex flex-col gap-3">
                   <Card.Header>
                     <h3 className="text-xl font-semibold text-gray-800">{name}</h3>
@@ -130,7 +151,10 @@ function App() {
                   </Card.Body>
 
                   <Card.Footer className="text-sm text-yellow-500 font-medium flex items-center gap-1">
-                    <Button className="w-full bg-[#002B56] text-white py-3 cursor-pointer hover:bg-[#002b56f5] transition-all duration-200" onClick={() => navigate(`/detail/${id}`)}>
+                    <Button
+                      className="w-full bg-[#002B56] text-white py-3 cursor-pointer hover:bg-[#002b56f5] transition-all duration-200"
+                      onClick={() => navigate(`/detail/${id}`)}
+                    >
                       Learn More
                     </Button>
                   </Card.Footer>
@@ -140,11 +164,16 @@ function App() {
           })}
         </div>
 
-        <div className="w-lg mx-auto">
-          <Button onClick={handleLoadMore} className="w-full py-3 text-base font-medium border border-[#002b56] rounded-md uppercase cursor-pointer hover:bg-gray-50 text-[#002b56]">
-            Load More
-          </Button>
-        </div>
+        {filteredByRating.length > itemsToShow && (
+          <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 mx-auto">
+            <Button
+              onClick={handleLoadMore}
+              className="w-full py-3 text-base font-medium border border-[#002b56] rounded-md uppercase cursor-pointer hover:bg-gray-50 text-[#002b56]"
+            >
+              Load More
+            </Button>
+          </div>
+        )}
       </section>
     </main>
   );
--- src/pages/_id/index.jsx
+++ src/pages/_id/index.jsx
@@ -21,22 +21,21 @@ function DetailPage() {
   const restaurant = restaurantDetail.restaurant;
   let staticCoordinate = [-6.466009, 106.848309];
 
-  console.log(restaurant);
-
   return (
-    <main className="container mx-auto px-4 md:px-8 py-10">
-      <Card className="flex flex-col md:flex-row gap-8 overflow-hidden">
-        <Card.Image src={RestaurantService.getImageUrl(restaurant.pictureId, 'large')} alt={restaurant.name} className="w-full h-64 md:h-full object-cover" />
+    <main className="min-h-screen max-w-screen-xl mx-auto px-4 sm:px-6 md:px-8 lg:px-10 py-10 flex flex-col gap-10">
+      <Card className="flex flex-col lg:flex-row md:flex-col gap-6 md:gap-8 overflow-hidden">
+        <Card.Image src={RestaurantService.getImageUrl(restaurant.pictureId, 'large')} alt={restaurant.name} className="w-full h-full object-cover" />
 
-        <Card.Header className="w-full md:w-1/2 lg:p-8 md:p-6 px-2 flex flex-col justify-between space-y-6">
-          <Card.Body className="space-y-4">
-            <span className="flex items-center gap-1">
+        <Card.Header className="w-full lg:w-1/2 md:w-full flex flex-col justify-between p-4 sm:p-6 md:p-3 gap-6">
+          <Card.Body className="flex flex-col gap-4">
+            <span className="flex items-center gap-1 text-sm text-gray-600">
               <MapPin size={18} />
               {restaurant?.address}
             </span>
-            <h1 className="text-3xl lg:text-5xl md:text-4xl font-bold text-gray-900 leading-tight">{restaurant.name}</h1>
 
-            <div className="flex items-center justify-between text-sm text-gray-600">
+            <h1 className="text-3xl sm:text-4xl lg:text-5xl font-bold text-gray-900 leading-tight">{restaurant.name}</h1>
+
+            <div className="flex justify-between w-full sm:flex-row sm:justify-between text-sm text-gray-600 gap-2">
               <div className="flex items-center gap-2">
                 <span className="uppercase font-semibold tracking-wide">{restaurant.city}</span>
                 <span className="text-yellow-600 font-medium">{restaurant.rating.toFixed(1)} / 5</span>
@@ -49,17 +48,17 @@ function DetailPage() {
 
             <div className="flex gap-1">
               {Array.from({ length: 5 }).map((_, i) => (
-                <Star key={i} size={18} className={`${i < Math.floor(restaurant.rating) ? 'fill-yellow-500 text-yellow-500' : 'text-gray-300'}`} />
+                <Star key={i} size={18} className={i < Math.floor(restaurant.rating) ? 'fill-yellow-500 text-yellow-500' : 'text-gray-300'} />
               ))}
             </div>
 
-            <p className="text-gray-700 text-base leading-relaxed text-justify">{restaurant.description}</p>
+            <p className="text-gray-700 text-xs leading-relaxed text-justify">{restaurant.description}</p>
           </Card.Body>
 
           <Card.Footer className="flex flex-wrap gap-2 pt-2">
             {restaurant.categories.map((cat, index) => (
               <Card.Badge key={index} className="inline-flex items-center gap-1.5 bg-[#002b56] text-white text-xs px-3 py-1.5 rounded-full">
-                <Tag size={14} className="text-white" />
+                <Tag size={14} />
                 {cat.name}
               </Card.Badge>
             ))}
@@ -67,36 +66,34 @@ function DetailPage() {
         </Card.Header>
       </Card>
 
-      <Card className="mt-10 p-6 space-y-6">
-        <h2 className="text-3xl font-semibold text-gray-900">Customer Reviews</h2>
+      <Card className="p-4 sm:p-6 space-y-6">
+        <h2 className="text-2xl sm:text-3xl font-semibold text-gray-900">Customer Reviews</h2>
 
         <div className="space-y-4">
           {restaurant.customerReviews.map((review, index) => (
-            <div key={index} className="flex items-center gap-4 p-4 bg-gray-50 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 ease-in-out">
+            <div key={index} className="flex items-start gap-4 p-4 bg-gray-50 rounded-lg shadow-sm hover:shadow-md transition duration-300">
               <div className="w-12 h-12 rounded-full bg-[#002b56] text-white flex items-center justify-center text-lg font-semibold">{review.name.charAt(0)}</div>
-
               <div className="flex-1">
-                <div className="flex items-center justify-between mb-2">
+                <div className="flex items-center justify-between mb-1">
                   <span className="font-semibold text-gray-800">{review.name}</span>
                   <span className="text-xs text-gray-500">{review.date}</span>
                 </div>
-
-                <p className="text-gray-700 text-sm leading-relaxed">{review.review}</p>
+                <p className="text-gray-700 text-sm">{review.review}</p>
               </div>
             </div>
           ))}
         </div>
       </Card>
 
-      <Card className="mt-10 p-6 space-y-6">
-        <div className="w-full rounded-lg overflow-hidden">
-          <MapContainer center={staticCoordinate} zoom={13} className="h-[500px]">
-            <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      <Card className="p-4 sm:p-6">
+        <h2 className="text-2xl sm:text-3xl font-semibold text-gray-900 mb-4">Location</h2>
 
+        <div className="w-full h-[300px] sm:h-[400px] md:h-[500px] rounded-lg overflow-hidden">
+          <MapContainer center={staticCoordinate} zoom={13} className="h-full w-full">
+            <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
             <MarkerClusterGroup chunkedLoading iconCreateFunction={createClusterCustomIcon}>
               {markers.map((marker) => (
                 <Marker key={marker.name} position={marker.geoPosition}>
-                  <p>{marker.name}</p>
                   <Popup>{marker.popUp}</Popup>
                 </Marker>
               ))}
